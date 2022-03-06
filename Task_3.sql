7af3a1bfbce3b6e38efec50fff973ba2d1503ad6
nimranasir29 <64038512+nimranasir29@users.noreply.github.com>
2022-03-06 16:44:23 +0500

    Lab_2_Task_3
    Using the DELETE statement, delete the record for the student having name Akram and Ahsan in the above table. Also delete the record for the course having course code=1001.

--- Task_3.sql
+++ Task_3.sql
@@ -1,4 +1,4 @@
-CREATE TABLE PersonsLab3 (
+CREATE TABLE PersonsLab2 (
 Name varchar(255),
 Reg_No int,
 Courses varchar (255),
@@ -6,51 +6,48 @@ Courses_Code int,
 Offered_By varchar(255)
 );
 
-select count(Courses) AS Column3 from PersonsLab3
-select count(Courses_Code) AS Column4  from PersonsLab3
-Select count(Offered_By) AS Column5 From PersonsLab3
-select count(*) as total_Courses  from PersonsLab3
-where Courses = 'DIP'
+Select * from PersonsLab2;
 
-Select upper(Name) AS  NAME from PersonsLab3
-Select upper(Courses) AS  COURSES from PersonsLab3
-Select upper(Offered_By) AS  OFFERED_BY from PersonsLab3
-
-Select lower(Name) AS  name from PersonsLab3
-Select lower(Courses) AS  courses from PersonsLab3
-Select lower(Offered_By) AS  offered_by from PersonsLab3
-
-Select Courses from PersonsLab3 GROUP BY Courses
-
-insert into PersonsLab3 Values
+insert into PersonsLab2 Values
 ('Ali',01,'DIP',1001,'Mr.A');
 
-insert into PersonsLab3 Values
+insert into PersonsLab2 Values
 ('Basit',02,'DBMS',1002,'Mr.X');
 
-insert into PersonsLab3 Values
+insert into PersonsLab2 Values
 ('Akram',03,'OS',1003,'Mr.Y');
 
-insert into PersonsLab3 Values
+insert into PersonsLab2 Values
 ('Asad',04,'DBMS',1002,'Mr.X');
 
-insert into PersonsLab3 Values
+insert into PersonsLab2 Values
 ('Zeeshan',05,'DIP',1001,'Mr.A');
 
-insert into PersonsLab3  (Name, Reg_No, Courses,Offered_By)
-values('Muneer',06,'OS','Mr.Y');
+insert into PersonsLab2 Values
+('Muneer',06,'OS',1003,'Mr.Y');
 
-insert into PersonsLab3 Values
+insert into PersonsLab2 Values
 ('Shafqat',07,'NM',1004,'Mr.H');
 
-insert into PersonsLab3 Values
+insert into PersonsLab2 Values
 ('Ahsan',08,'OS',1003,'Mr.Y');
 
-insert into PersonsLab3 (Name, Reg_No, Courses,Offered_By)
-values('Ikram',09,'DIP','Mr.A');
 
-insert into PersonsLab3 (Name, Reg_No,Courses)
-values('Hassan',10,'DSP');
+insert into PersonsLab2 (Name, Reg_No, Courses)
+values('Ikram',09,'DIP');
 
-Select * from PersonsLab3;
-delete from PersonsLab3
+insert into PersonsLab2 (Name, Reg_No)
+values('Hassan',10);
+
+update PersonsLab2 SET Courses_Code= 1001 , Offered_By='Mr.A'
+WHERE Name = 'Ikram';
+
+update PersonsLab2 SET Courses = 'DSP',Courses_Code= 1005 , Offered_By='Mr.Z'
+WHERE Name = 'Hassan';
+
+DELETE FROM PersonsLab2
+Where Name = 'Akram' OR NAME= 'Ahsan'
+
+DELETE FROM PersonsLab2
+Where Courses_Code = 1001
+
